912fae63f2d67e52cb2179f976640526a6765820
tuutuu <tuure.piitulainen@gmail.com>
2019-01-21 19:04:02 +0200

    1.7

--- osa1/osa1_unicafe/src/index.js
+++ osa1/osa1_unicafe/src/index.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistiikka = (props) => {
+const Total = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.good + props.neutral + props.bad}</p>
+        </div>
+    )
+}
+
+const Average = (props) => {
+    return (
+        <div>
+            <p>keskiarvo {props.good - props.bad / (props.good + props.neutral + props.bad)}</p>
+        </div>
+    )
+}
+
+const GoodPortion = (props) => {
+    return (
+        <div>
+            <p>positiivisia {100.0 * (props.good/(props.good + props.neutral + props.bad))}</p>
+        </div>
+    )
+}
+
+const Statistics = (props) => {
     return (
         <div>
             <h1>statistiikka</h1>
             <p>hyvä {props.good}</p>
             <p>neutraali {props.neutral}</p>
             <p>huono {props.bad}</p>
+            <Total good={props.good} neutral={props.neutral} bad={props.bad} />
+            <Average good={props.good} neutral={props.neutral} bad={props.bad} />
+            <GoodPortion good={props.good} neutral={props.neutral} bad={props.bad} />
         </div>
     )
 }
@@ -29,7 +56,7 @@ const App = () => {
             <button onClick={() => setBad(bad + 1)}>
                 huono
             </button>
-            <Statistiikka good={good} neutral={neutral} bad={bad} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }

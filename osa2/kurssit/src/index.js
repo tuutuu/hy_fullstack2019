179e960e36fa506ee7e271921500d226a8f6fb2f
tuutuu <tuure.piitulainen@gmail.com>
2019-02-04 12:13:53 +0200

    2.1

--- osa2/kurssit/src/index.js
+++ osa2/kurssit/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+    </div>
+  )
+}
+
 const Header = props =>
-  <h1>{props.course}</h1>
+  <h1>{props.course.name}</h1>
 
 const Total = props => {
   const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
@@ -16,9 +25,7 @@ const Part = props =>
 
 const Content = props => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.course.parts.map(part => <Part key={part.id} part={part} />)}
   </div>
 )
 
@@ -28,24 +35,30 @@ const App = () => {
     parts: [
       {
         name: 'Reactin perusteet',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Tiedonvälitys propseilla',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'Komponenttien tila',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },      
+      {
+        name: 'Windows 95:n perusteet',
+        exercises: 10,
+        id: 4
       }
     ]
-  } 
+  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }

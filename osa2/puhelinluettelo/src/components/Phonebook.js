972f3fd4e538528516d283a2ad7a9719f1ebd555
tuutuu <tuure.piitulainen@gmail.com>
2019-02-04 21:05:55 +0200

    Confirm window

--- osa2/puhelinluettelo/src/components/Phonebook.js
+++ osa2/puhelinluettelo/src/components/Phonebook.js
@@ -4,18 +4,20 @@ import personService from '../services/persons'
 const Phonebook = ({ persons, setPersons, newFilter }) => {
     const personsToShow = persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase()))
 
-    const handleOnClick = (id) => {
-        personService
-            .remove(id)
-            .then(response => {
-                setPersons(persons.filter(person => person.id !== id))
-            })
+    const handleOnClick = (id, name) => {
+        if (window.confirm(`Poistetaanko ${name}?`)) {
+            personService
+                .remove(id)
+                .then(response => {
+                    setPersons(persons.filter(person => person.id !== id))
+                })
+        }
     }
 
     const rows = () => personsToShow.map(person =>
         <li key={person.name}>
             {person.name} {person.number}
-            <button onClick={() => handleOnClick(person.id)}>
+            <button onClick={() => handleOnClick(person.id, person.name)}>
                 poista
             </button>
         </li>

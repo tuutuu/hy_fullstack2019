2383a4fe57de74491c2131135b4da98626db2ac7
tuure <tuure.piitulainen@gmail.com>
2019-02-18 10:12:06 +0200

    4.2

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,26 +1,13 @@
 const config = require('./utils/config')
 const http = require('http')
 const app = require('./app')
-const cors = require('cors')
-const mongoose = require('mongoose')
+// const cors = require('cors')
 
 /*
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-
-
-
 app.use(cors())
 app.use(bodyParser.json())
-
 */
+
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {

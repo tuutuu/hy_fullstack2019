f999a2e5064b1de8695f4ea7bd316a98d3b2b4e0
tuure <tuure.piitulainen@gmail.com>
2019-02-18 11:43:03 +0200

    more comfortable

--- osa4/bloglist/.eslintrc.js
+++ osa4/bloglist/.eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
       "arrow-spacing": [
           "error", { "before": true, "after": true }
       ],
-      "no-console": 0
+      "no-console": 0,
+      "no-unused-vars": 0
   }
 };

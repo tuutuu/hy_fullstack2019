ee0f5ddea6086a889ef71c04e84d41dbc287b607
tuure <tuure.piitulainen@gmail.com>
2019-02-18 17:22:20 +0200

    logger

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -5,15 +5,16 @@ const app = express()
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(express.static('build'))

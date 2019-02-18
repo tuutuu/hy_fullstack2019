12f198199ad3112acf3aa18dd299141cb476142e
tuure <tuure.piitulainen@gmail.com>
2019-02-18 19:22:55 +0200

    4.10

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -3,65 +3,17 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }
-]
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
   let blogObject = {}
 
-  for (let i = 0; i < initialBlogs.length; i++) {
-    blogObject = new Blog(initialBlogs[i])
+  for (let i = 0; i < helper.initialBlogs.length; i++) {
+    blogObject = new Blog(helper.initialBlogs[i])
     await blogObject.save()
   }
 })
@@ -69,7 +21,25 @@ beforeEach(async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(initialBlogs.length)
+  expect(response.body.length).toBe(helper.initialBlogs.length)
+})
+
+test('a blog can be added', async () => {
+  const newBlog = {
+    title: "Dijkstra's New Blog about Dogs",
+    author: "Edsger W. Dijkstra",
+    url: "www.dog.com/dijkstra",
+    likes: 300
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
 })
 
 afterAll(() => {

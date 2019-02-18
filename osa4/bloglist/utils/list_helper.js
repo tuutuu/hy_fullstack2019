44c92be6bf1026b26fa59bab4c868f8b2a5e9dbf
tuure <tuure.piitulainen@gmail.com>
2019-02-18 12:20:38 +0200

    4.5

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -3,12 +3,23 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  let total = blogs.reduce((prev, cur) => {
-    return prev + cur.likes
+  let total = blogs.reduce((acc, cur) => {
+    return acc + cur.likes
   }, 0)
   return total
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = blogs.reduce((acc, cur) => {
+    return(
+      acc.likes > cur.likes
+        ? acc
+        : cur
+    )
+  })
+  return favorite
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }

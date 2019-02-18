6bf70604dcc624b64082105306cbb3fd33395bbe
tuure <tuure.piitulainen@gmail.com>
2019-02-18 17:22:09 +0200

    testing

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -11,6 +11,18 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there is one blog', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(1)
+})
+
+test('the first blog is about types', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].title).toBe('Type wars')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })

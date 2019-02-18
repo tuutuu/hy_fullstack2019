c10464f4fcd21d975b395bc9566219f9732a85f7
tuure <tuure.piitulainen@gmail.com>
2019-02-18 10:10:26 +0200

    model

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,11 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 const config = require('./utils/config')
 const http = require('http')
 const app = require('./app')
-const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
@@ -19,38 +14,12 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
 
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 */
 const server = http.createServer(app)
 

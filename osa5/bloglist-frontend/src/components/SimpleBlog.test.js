1b232d78f22b373e5e58ace5b5c1b3d2e15f16b3
tuure <tuure.piitulainen@gmail.com>
2019-02-25 17:52:33 +0200

    unused

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import 'jest-dom/extend-expect'
 import { render, cleanup } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
-
-afterEach(cleanup)
 
 describe('SimpleBlog', () => {
   let blog

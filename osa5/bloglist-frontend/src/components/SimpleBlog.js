4538f64576881025e12902640a1041621f0b498b
tuure <tuure.piitulainen@gmail.com>
2019-02-25 18:18:03 +0200

    5.14

--- osa5/bloglist-frontend/src/components/SimpleBlog.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const SimpleBlog = ({ blog, onClick }) => (
-  <div className='blog'>
+  <div>
     <div>
       {blog.title} {blog.author}
     </div>
     <div>
       blog has {blog.likes} likes
-      <button onClick={onClick}>like</button>
+      <button className='likeButton' onClick={onClick}>like</button>
     </div>
   </div>
 )
--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { render, cleanup } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 describe('SimpleBlog', () => {
   let blog
   let component
+  let mockHandler
 
   beforeEach(() => {
+    mockHandler = jest.fn()
+
     blog = {
       title: "Komponenttitestauksen syvimmät salat",
       author: "Jukka Palmu",
@@ -14,25 +17,34 @@ describe('SimpleBlog', () => {
     }
 
     component = render(
-      <SimpleBlog blog={blog} />
+      <SimpleBlog blog={blog} onClick={mockHandler} />
     )
   })
 
-  test('renders title', () => {
+  it('renders title', () => {
     expect(component.container).toHaveTextContent(
       'Komponenttitestauksen syvimmät salat'
     )
   })
 
-  test('renders author', () => {
+  it('renders author', () => {
     expect(component.container).toHaveTextContent(
       'Jukka Palmu'
     )
   })
 
-  test('renders likes', () => {
+  it('renders likes', () => {
     expect(component.container).toHaveTextContent(
       `blog has ${blog.likes} likes`
     )
+  })
+
+
+  it('clicking the like button twice calls event handler twice', async () => {
+    const button = component.getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })

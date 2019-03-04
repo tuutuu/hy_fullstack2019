5f5c1da56860dd5233c872a471dffa55dc0f5e43
tuure <tuure.piitulainen@gmail.com>
2019-03-04 15:14:00 +0200

    6.5*

--- osa6/anecdotes-redux/src/components/AnecdoteList.js
+++ osa6/anecdotes-redux/src/components/AnecdoteList.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = ({ store }) => {
-  const anecdotes = store.getState()
+  const anecdotes = store.getState().sort((a, b) => {
+    if(a.content < b.content) { return -1 }
+    if(a.content > b.content) { return 1 }
+    return 0
+  })
 
   const vote = (id) => {
     store.dispatch(voteAnecdote(id))

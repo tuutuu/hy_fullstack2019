bff0e80345dc38c302c4071fa0c62e0cffe23cc6
tuure <tuure.piitulainen@gmail.com>
2019-03-04 15:08:30 +0200

    6.8

--- osa6/anecdotes-redux/src/App.js
+++ osa6/anecdotes-redux/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 
 const App = ({ store }) => {
-  const anecdotes = store.getState()
-
-  const vote = (id) => {
-    store.dispatch(voteAnecdote(id))
-  }
-
+  
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={store} />
       <AnecdoteForm store={store} />
     </div>
   )

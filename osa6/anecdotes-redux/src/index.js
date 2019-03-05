a7ce1466b372e2e9fe074cf127f9947719020975
tuure <tuure.piitulainen@gmail.com>
2019-03-05 11:03:50 +0200

    Fixed vote

--- osa6/anecdotes-redux/src/index.js
+++ osa6/anecdotes-redux/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import App from './App'
 import anecdoteReducer, { initAnecdotes } from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import { Provider } from 'react-redux'
 import anecdoteService from './services/anecdotes'
+import thunk from 'redux-thunk'
 
 anecdoteService.getAll().then(anecdotes =>
   store.dispatch(initAnecdotes(anecdotes))
@@ -16,7 +17,7 @@ const reducer = combineReducers({
   notification: notificationReducer
 })
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 const render = () => {
   ReactDOM.render(
